d9ff3407cf9088565e1c4d7b3cb0aefaa89d8234
AlekseyLu <luzyanin-ae@yandex.ru>
2024-11-19 11:15:11 +0300

    fix certificate size

--- src/script/app.js
+++ src/script/app.js
@@ -27,100 +27,6 @@ function createVideoElement(url) {
   return video;
 }
 
-/**
- * <li class="grid grid-cols-2 justify-items-center mb-20 gap-8">
-      <div
-        class="sm:col-span-1 sm:mb-0 mb-4 col-span-2 sm:text-left text-center max-w-md content-center justify-items-center">
-        <h3 class="mb-5 font-bold text-3xl lg:text-5xl">
-          Видео с нашего проката летом
-        </h3>
-        <p class="font-normal">
-          Посмотрите видео, и попробуйте ощутить положительные эмоции и
-          почувствовать скорость.
-        </p>
-      </div>
-      <div
-        class="group relative sm:col-span-1 col-span-2 content-center justify-items-center rounded-lg overflow-hidden shadow-xl shadow-black/60">
-        <img src="./images/for-video-summer.png" alt="123" class="w-full h-full object-cover">
-        <div
-          class="absolute lg:top-[120%] top-0 transition-all lg:group-hover:top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-black/40">
-          <button data-id="videoBtn" class="font-black text-white">Watch now</button>
-        </div>
-      </div>
-    </li>
- */
-
-// function createCertificateElement(
-//   imageUrl,
-//   titleTextContent,
-//   descriptionTextContent,
-//   ind
-// ) {
-//   const evenNumber = ind % 2 === 0;
-//   const li = document.createElement("li");
-//   const descriptionContainer = document.createElement("div");
-//   const descriptionTitle = document.createElement("h3");
-//   const descriptionText = document.createElement("p");
-//   const imageContainer = document.createElement("div");
-//   const image = document.createElement("img");
-//   const link = document.createElement("a");
-
-//   li.classList.add(
-//     "grid",
-//     "grid-cols-2",
-//     "justify-items-center",
-//     "mb-20",
-//     "gap-8"
-//   );
-//   descriptionContainer.classList.add(
-//     "sm:col-span-1",
-//     "sm:mb-0",
-//     "mb-4",
-//     "col-span-2",
-//     "sm:text-left",
-//     "text-center",
-//     "max-w-md",
-//     "content-center",
-//     "justify-items-center"
-//   );
-//   descriptionTitle.classList.add(
-//     "mb-5",
-//     "font-bold",
-//     "text-3xl",
-//     "lg:text-5xl"
-//   );
-//   descriptionText.classList.add("font-normal");
-//   imageContainer.classList.add(
-//     "relative",
-//     "sm:col-span-1",
-//     "col-span-2",
-//     "content-center",
-//     "justify-items-center",
-//     "rounded-lg",
-//     "overflow-hidden",
-//     "shadow-xl",
-//     "shadow-black/60"
-//   );
-//   link.classList.add();
-//   descriptionTitle.textContent = titleTextContent;
-//   descriptionText.textContent = descriptionTextContent;
-//   image.classList.add("w-full", "h-full", "object-cover");
-//   image.setAttribute("src", imageUrl);
-//   image.setAttribute("alt", titleTextContent);
-//   descriptionContainer.append(descriptionTitle, descriptionText);
-//   imageContainer.append(image);
-//   const evenArr = [descriptionContainer, imageContainer];
-//   const notEvenArr = [imageContainer, descriptionContainer];
-
-//   evenNumber ? li.append(...evenArr) : li.append(...notEvenArr);
-
-//   return li;
-// }
-
-/**
- * create cert with 2 coloms
- */
-
 function createCustomElement(item, classes) {
   const element = document.createElement(item);
   if (classes.length > 0) {
@@ -142,6 +48,7 @@ function createCertificateElement(
     "grid",
     "grid-cols-3",
     "min-h-60",
+    "max-h-64",
     "rounded-lg",
     "overflow-hidden",
     "shadow-md",
